c13448eb8afb86f5faa4e0c0e388d83d91e4e5a9
bonyubking <piusku2@hanyang.ac.kr>
2025-12-18 20:44:05 -0800

    "initialize project"

---
+++ src/main/java/com/myce/notifcation/MyceNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.myce.notifcation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyceNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyceNotificationApplication.class, args);
+    }
+
+}
