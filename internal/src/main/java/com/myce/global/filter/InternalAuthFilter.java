ed2772581a5998b8e5f23c0ec28c162626975447
구본엽 <piusku2@gmail.com>
2026-01-22 11:47:24 +0900

    InternalAuthFilter 적용 -> Spring Security Internal module에서 SecurityConfig 삭제

--- internal/src/main/java/com/myce/global/filter/InternalAuthFilter.java
+++ internal/src/main/java/com/myce/global/filter/InternalAuthFilter.java
@@ -4,25 +4,27 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Slf4j
-@RequiredArgsConstructor
-public class JwtAuthenticationFilter extends OncePerRequestFilter {
+@Component
+public class InternalAuthFilter extends OncePerRequestFilter {
 
-    private final String GATEWAY_AUTH_VALUE;
+    @Value("${gateway.auth.value}")
+    private String internalAuthValue;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
 
         String authValue = request.getHeader(InternalHeaderKey.INTERNAL_AUTH);
-
-        if (authValue == null || (!authValue.equals(GATEWAY_AUTH_VALUE))) {
+        if (authValue == null || !authValue.equals(internalAuthValue)) {
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             return;
         }
