d01a2488385ae702c2ce45b279798d81c4a1ede1
구본엽 <piusku2@gmail.com>
2026-01-28 23:29:27 +0900

    Eureka LoadBalancing을 위한 설정 및 RestClient 요청에도 LB 적용

--- api/src/main/java/com/myce/notification/restclient/config/RestClientConfig.java
+++ api/src/main/java/com/myce/notification/restclient/config/RestClientConfig.java
@@ -3,6 +3,7 @@ package com.myce.notification.restclient.config;
 import com.myce.notification.exception.CustomErrorCode;
 import com.myce.notification.exception.CustomException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatusCode;
@@ -11,6 +12,12 @@ import org.springframework.web.client.RestClient;
 @Configuration
 public class RestClientConfig {
 
+    @Bean
+    @LoadBalanced
+    public RestClient.Builder loadBalancedRestClientBuilder() {
+        return RestClient.builder();
+    }
+
     @Value("${external.base-url.core}")
     private String coreBaseUrl;
 
@@ -18,8 +25,8 @@ public class RestClientConfig {
     private String externalAuthValue;
 
     @Bean(name = "coreClient")
-    public RestClient coreClient() {
-        return RestClient.builder()
+    public RestClient coreClient(RestClient.Builder loadBalancedRestClientBuilder) {
+        return loadBalancedRestClientBuilder
                 .baseUrl(coreBaseUrl)
                 .defaultHeader("X-Internal-Auth", externalAuthValue)
                 .build();
